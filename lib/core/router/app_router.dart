89aa54532ced98b3714b251f13dd14d6a1885450
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-08 22:02:02 +0200

    add onBoarding to router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/features/onboarding/presentaion/views/onboarding_view.dart';
 import 'package:e_commerce/features/splsh/presentation/views/splsh_view.dart';
 
 import 'package:go_router/go_router.dart';
@@ -9,8 +10,8 @@ final GoRouter router = GoRouter(
       builder: (context, state) => const SplshView(),
     ),
     GoRoute(
-      path: '/',
-      builder: (context, state) => const SplshView(),
+      path: 'onBoarding',
+      builder: (context, state) => const OnBoardingView(),
     ),
   ],
 );
