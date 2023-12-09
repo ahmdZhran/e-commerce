394865c53f8677fac4069756da87a1491a270198
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-09 16:04:55 +0200

    solve conflict between buttons

--- lib/features/onboarding/presentaion/widgets/get_buttons.dart
+++ lib/features/onboarding/presentaion/widgets/get_buttons.dart
@@ -1,4 +1,3 @@
-import 'package:e_commerce/core/functions/navigation_method.dart';
 import 'package:e_commerce/core/utls/app_strings.dart';
 import 'package:e_commerce/core/utls/texts_style.dart';
 import 'package:e_commerce/core/widgets/custom_button.dart';
@@ -16,20 +15,14 @@ class GetButtons extends StatelessWidget {
 
   final PageController _controller;
   final int currentIndex;
-  final VoidCallback onPressed;
+  final void Function() onPressed;
   @override
   Widget build(BuildContext context) {
     if (currentIndex == onBoardingData.length - 1) {
       return Column(
         children: [
           CustomButton(
-            onPressed: () {
-              _controller.nextPage(
-                duration: const Duration(milliseconds: 200),
-                curve: Curves.bounceIn,
-              );
-              customNavigation(context, '/SignUp');
-            },
+            onPressed: onPressed,
             text: Text(
               AppStrigns.getStarted,
               style: CustomTextStyle.semiBold16,
@@ -39,7 +32,11 @@ class GetButtons extends StatelessWidget {
       );
     } else {
       return CustomButton(
-        onPressed: onPressed,
+        onPressed: () {
+          _controller.nextPage(
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.bounceIn);
+        },
         text: Text(
           AppStrigns.next,
           style: CustomTextStyle.semiBold16,
