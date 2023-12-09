f0c538336881adbfa1f223c471d4e45a34cd3ed5
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-09 16:25:08 +0200

    refactor code

--- lib/app/e_commerce_app.dart
+++ lib/app/e_commerce_app.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/core/router/app_router.dart';
 import 'package:e_commerce/core/utls/app_color.dart';
+import 'package:e_commerce/core/utls/app_strings.dart';
 import 'package:flutter/material.dart';
 
 class Ecommerce extends StatelessWidget {
@@ -9,7 +10,7 @@ class Ecommerce extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: AppStrigns.appName,
       theme: ThemeData(
         scaffoldBackgroundColor: AppColors.kBlackColor,
         useMaterial3: true,
