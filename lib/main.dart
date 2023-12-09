ab481c033a65d05745cdf0b6426a8c6eda111b28
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-09 15:45:22 +0200

    do ensureInitailzed for app in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:e_commerce/app/e_commerce_app.dart';
+import 'package:e_commerce/core/database/cache_helper.dart';
+import 'package:e_commerce/core/services/services_locator.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  setUpSeviceLocator();
+  await getIt<CacheHelper>().init();
   runApp(const Ecommerce());
 }
