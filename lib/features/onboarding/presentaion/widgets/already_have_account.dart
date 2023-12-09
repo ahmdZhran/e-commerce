405c7248f4fd14cb58879947bf815a3e0317c7b0
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-09 11:42:22 +0200

    onboarding view done

---
+++ lib/features/onboarding/presentaion/widgets/already_have_account.dart
@@ -0,0 +1,27 @@
+import 'package:e_commerce/core/utls/app_strings.dart';
+import 'package:e_commerce/core/utls/texts_style.dart';
+import 'package:flutter/material.dart';
+
+class AlreadyHaveAccountWidget extends StatelessWidget {
+  const AlreadyHaveAccountWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          AppStrigns.alreadyHaveAccount,
+          style: CustomTextStyle.regular14,
+        ),
+        const SizedBox(width: 3),
+        Text(
+          AppStrigns.login,
+          style: CustomTextStyle.medium14,
+        ),
+      ],
+    );
+  }
+}
--- lib/features/onboarding/presentaion/widgets/get_buttons.dart
+++ lib/features/onboarding/presentaion/widgets/get_buttons.dart
@@ -2,62 +2,45 @@ import 'package:e_commerce/core/utls/app_strings.dart';
 import 'package:e_commerce/core/utls/texts_style.dart';
 import 'package:e_commerce/core/widgets/custom_button.dart';
 import 'package:e_commerce/features/onboarding/data/model/onboading_data.dart';
+
 import 'package:flutter/material.dart';
 
-class GetButtons extends StatefulWidget {
+class GetButtons extends StatelessWidget {
   const GetButtons({
     super.key,
     required PageController controller,
+    required this.currentIndex,
   }) : _controller = controller;
 
   final PageController _controller;
-
-  @override
-  State<GetButtons> createState() => _GetButtonsState();
-}
-
-class _GetButtonsState extends State<GetButtons> {
-  int currentIndix = 0;
-
+  final int currentIndex;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        currentIndix == onBoardingData.length - 1
-            ? CustomButton(
-                onPressed: () {},
-                text: Text(
-                  AppStrigns.getStarted,
-                  style: CustomTextStyle.semiBold16,
-                ),
-              )
-            : CustomButton(
-                onPressed: () {
-                  widget._controller.nextPage(
-                      duration: const Duration(milliseconds: 200),
-                      curve: Curves.bounceIn);
-                },
-                text: Text(
-                  AppStrigns.next,
-                  style: CustomTextStyle.semiBold16,
-                ),
-              ),
-        const SizedBox(height: 30),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              AppStrigns.alreadyHaveAccount,
-              style: CustomTextStyle.regular14,
+    if (currentIndex == onBoardingData.length - 1) {
+      return Column(
+        children: [
+          CustomButton(
+            onPressed: () {
+              _controller.nextPage(
+                  duration: const Duration(milliseconds: 200),
+                  curve: Curves.bounceIn);
+            },
+            text: Text(
+              AppStrigns.getStarted,
+              style: CustomTextStyle.semiBold16,
             ),
-            const SizedBox(width: 3),
-            Text(
-              AppStrigns.login,
-              style: CustomTextStyle.medium14,
-            ),
-          ],
-        ),
-      ],
-    );
+          ),
+        ],
+      );
+    } else {
+      return CustomButton(
+        onPressed: () {
+          _controller.nextPage(
+              duration: const Duration(milliseconds: 200),
+              curve: Curves.bounceIn);
+        },
+        text: const Text(AppStrigns.next),
+      );
+    }
   }
 }
