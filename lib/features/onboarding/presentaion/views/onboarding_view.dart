b1ad4a1da1264fa04235b1854fe15dfbae3ecaa5
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-09 08:24:21 +0200

    delete unnecessary import files

--- lib/features/onboarding/presentaion/views/onboarding_view.dart
+++ lib/features/onboarding/presentaion/views/onboarding_view.dart
@@ -1,9 +1,6 @@
-import 'package:e_commerce/core/utls/app_color.dart';
-import 'package:e_commerce/features/onboarding/data/model/onboading_data.dart';
 import 'package:e_commerce/features/onboarding/presentaion/widgets/cusomt_smooth_page_indicatro.dart';
 import 'package:e_commerce/features/onboarding/presentaion/widgets/onboarding_images.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnBoardingView extends StatelessWidget {
   OnBoardingView({super.key});
