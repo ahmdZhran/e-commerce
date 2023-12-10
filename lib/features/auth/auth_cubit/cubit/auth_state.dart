5ec3d266892a0c613f2ebedb91d507a590b5ddf8
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-10 15:58:04 +0200

    add states to auth state

--- lib/features/auth/auth_cubit/cubit/auth_state.dart
+++ lib/features/auth/auth_cubit/cubit/auth_state.dart
@@ -4,3 +4,9 @@ part of 'auth_cubit.dart';
 sealed class AuthState {}
 
 final class AuthInitial extends AuthState {}
+
+final class AuthLoading extends AuthState {}
+
+final class AuthSuccess extends AuthState {}
+
+final class AuthFailer extends AuthState {}
