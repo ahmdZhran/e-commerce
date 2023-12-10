d11f671fe8edc68d8d4045fed61a7826d21d9a40
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-10 13:03:44 +0200

    add strings to appStrings

--- lib/features/auth/presentation/widgets/form_for_sign_in.dart
+++ lib/features/auth/presentation/widgets/form_for_sign_in.dart
@@ -2,6 +2,7 @@ import 'package:e_commerce/core/utls/app_strings.dart';
 import 'package:e_commerce/core/utls/texts_style.dart';
 import 'package:e_commerce/core/widgets/custom_button.dart';
 import 'package:e_commerce/features/auth/presentation/widgets/custom_text_form_field.dart';
+
 import 'package:flutter/material.dart';
 
 class FormForSignInWidget extends StatelessWidget {
@@ -10,18 +11,32 @@ class FormForSignInWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Form(
-      child: Column(children: [
-        const CustomTextFomField(lableText: AppStrigns.emailAdress),
-        const SizedBox(height: 40),
-        const CustomTextFomField(lableText: AppStrigns.password),
-        const SizedBox(height: 40),
-        CustomButton(
+      child: Column(
+        children: [
+          const CustomTextFomField(lableText: AppStrigns.emailAdress),
+          const SizedBox(height: 40),
+          const CustomTextFomField(lableText: AppStrigns.password),
+          const SizedBox(height: 40),
+          CustomButton(
             onPressed: () {},
             text: Text(
               AppStrigns.login,
               style: CustomTextStyle.semiBold16,
-            )),
-      ]),
+            ),
+          ),
+          
+        ],
+      ),
     );
+  }
+}
+class DontHaveAnAccountWidget extends StatelessWidget {
+  const DontHaveAnAccountWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Text(App)
+    ],)
   }
 }
