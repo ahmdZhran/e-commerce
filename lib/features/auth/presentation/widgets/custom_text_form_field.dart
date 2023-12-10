ab310b3e53fc8f65459875af91fe545cb61defa7
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-10 10:49:36 +0200

    add custom text form field widget

---
+++ lib/features/auth/presentation/widgets/custom_text_form_field.dart
@@ -0,0 +1,34 @@
+import 'package:e_commerce/core/utls/texts_style.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFomField extends StatelessWidget {
+  const CustomTextFomField(
+      {super.key, required this.lableText, this.onChanged});
+  final String lableText;
+  final void Function(String)? onChanged;
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'this field is required';
+        } else {
+          return null;
+        }
+      },
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        enabledBorder: outlineInputBorder(),
+        focusedBorder: outlineInputBorder(),
+        labelText: lableText,
+        labelStyle: CustomTextStyle.regular14,
+      ),
+    );
+  }
+}
+
+OutlineInputBorder outlineInputBorder() {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(4),
+  );
+}
