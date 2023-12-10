a44b02d9911039e3357eae236e81bdfcea2ba820
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-10 12:14:13 +0200

    create sign in view

--- lib/features/auth/presentation/view/sign_in_view.dart
+++ lib/features/auth/presentation/view/sign_in_view.dart
@@ -1,10 +1,33 @@
+import 'package:e_commerce/core/utls/app_assets.dart';
+import 'package:e_commerce/core/utls/app_strings.dart';
+
+import 'package:e_commerce/features/auth/presentation/widgets/form_for_sign_in.dart';
+import 'package:e_commerce/features/auth/presentation/widgets/head_titles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverToBoxAdapter(
+            child: SizedBox(height: 150),
+          ),
+          SliverToBoxAdapter(
+            child: SvgPicture.asset(AppAssets.shopLogo),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 100)),
+          const SliverToBoxAdapter(
+            child: HeadTitles(text: AppStrigns.wecomeBack),
+          ),
+          const SliverToBoxAdapter(child: SizedBox(height: 40)),
+          const SliverToBoxAdapter(child: FormForSignInWidget()),
+        ],
+      ),
+    );
   }
 }
