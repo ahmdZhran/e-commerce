45dcde396989d27ac76608d246deecfabd4301ec
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-10 16:12:50 +0200

    add body data

--- lib/features/auth/auth_cubit/cubit/auth_cubit.dart
+++ lib/features/auth/auth_cubit/cubit/auth_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:e_commerce/core/network/remote/dio_helper.dart';
+import 'package:e_commerce/core/utls/constants.dart';
 import 'package:e_commerce/features/auth/data/models/user_model.dart';
 import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
@@ -15,7 +16,13 @@ class AuthCubit extends Cubit<AuthState> {
   String? phone;
   GlobalKey<FormState> singInFormKey = GlobalKey();
   UserModel? userModel;
-  void userRegister(name,email,phone,nationalId,password){
-    DioHelperStore.postData(url: url, data: data)
+  void userRegister(name, email, phone, nationalId, password) {
+    DioHelperStore.postData(url: ApiConstants.registerApi, data: {
+      "name": name,
+      "email": email,
+      "phone": phone,
+      "nationalId": nationalId,
+      "password": password,
+    });
   }
 }
