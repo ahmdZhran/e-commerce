b8ffd3fe127accd00bb408c84395d38740304c08
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-10 13:57:31 +0200

    add variables in auth cubit

--- lib/features/auth/auth_cubit/cubit/auth_cubit.dart
+++ lib/features/auth/auth_cubit/cubit/auth_cubit.dart
@@ -1,8 +1,15 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
+  String? emailAddress;
+  String? password;
+  String? name;
+  String? nationalId;
+  String? phone;
+  GlobalKey<FormState> singInFormField = GlobalKey();
 }
