d586f81b88ec33cf4d707ba935bcb5dd2c0de9c8
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-17 20:38:20 +0200

    add comments

--- lib/features/auth/presentation/widgets/custom_form_for_signup.dart
+++ lib/features/auth/presentation/widgets/custom_form_for_signup.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:e_commerce/core/database/cache_helper.dart';
 import 'package:e_commerce/core/functions/navigation_method.dart';
 import 'package:e_commerce/core/utls/app_strings.dart';
@@ -8,21 +10,24 @@ import 'package:e_commerce/features/auth/auth_cubit/cubit/auth_cubit.dart';
 import 'package:e_commerce/features/auth/presentation/widgets/add_personal_photo.dart';
 import 'package:e_commerce/features/auth/presentation/widgets/custom_text_form_field.dart';
 import 'package:e_commerce/features/onboarding/presentaion/widgets/already_have_account.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CustomSignUpFormWidget extends StatelessWidget {
+  // Constructor with a key
   const CustomSignUpFormWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<AuthCubit, AuthState>(
       listener: (context, state) {
+        // Handle state changes
         if (state is RegisterSuccess) {
           if (state.userModel.status == "success") {
-            print("sucess");
+            // Successful registration
+            print("success");
             customNavigation(context, '/Sign in');
             print(state.userModel.user!.token);
+
+            // Save user data to cache
             CacheHelper()
                 .saveData(
                     key: 'userId', value: state.userModel.user!.nationalId)
@@ -36,19 +41,24 @@ class CustomSignUpFormWidget extends StatelessWidget {
               customNavigation(context, '/SignIn');
             });
           } else {
+            // Registration failed
             print(state.userModel.message);
           }
         }
       },
       builder: (context, state) {
+        // Get the AuthCubit instance
         var cubit = AuthCubit.get(context);
         AuthCubit authCubit = BlocProvider.of<AuthCubit>(context);
 
+        // Build the UI using a Form
         return Form(
-          key: authCubit.singUpFormKey,
+          key: authCubit.singUpFormKey, // Form key
           child: Column(children: [
             const AddPersonalPhotoWidget(),
             const SizedBox(height: 30),
+
+            // CustomTextFormField for full name
             CustomTextFomField(
                 onChanged: (name) {
                   authCubit.name = name;
@@ -56,6 +66,8 @@ class CustomSignUpFormWidget extends StatelessWidget {
                 lableText: AppStrigns.fullName,
                 keyboardType: TextInputType.name),
             const SizedBox(height: 16),
+
+            // CustomTextFormField for email
             CustomTextFomField(
                 onChanged: (email) {
                   authCubit.email = email;
@@ -63,6 +75,8 @@ class CustomSignUpFormWidget extends StatelessWidget {
                 lableText: AppStrigns.emailAdress,
                 keyboardType: TextInputType.emailAddress),
             const SizedBox(height: 16),
+
+            // CustomTextFormField for phone
             CustomTextFomField(
                 onChanged: (phone) {
                   authCubit.phone = phone;
@@ -70,6 +84,8 @@ class CustomSignUpFormWidget extends StatelessWidget {
                 lableText: AppStrigns.phone,
                 keyboardType: TextInputType.phone),
             const SizedBox(height: 16),
+
+            // CustomTextFormField for national ID
             CustomTextFomField(
                 onChanged: (nationId) {
                   authCubit.nationalId = nationId;
@@ -77,6 +93,8 @@ class CustomSignUpFormWidget extends StatelessWidget {
                 lableText: AppStrigns.nationalId,
                 keyboardType: TextInputType.number),
             const SizedBox(height: 16),
+
+            // CustomTextFormField for password
             CustomTextFomField(
                 onChanged: (password) {
                   authCubit.passowrd = password;
@@ -90,10 +108,14 @@ class CustomSignUpFormWidget extends StatelessWidget {
                 ),
                 lableText: AppStrigns.password),
             const SizedBox(height: 20),
+
+            // Show a loading indicator if the registration is in progress
             state is RegisterLoading
                 ? const CircularProgressIndicator()
+                // CustomButton for sign up
                 : CustomButton(
                     onPressed: () {
+                      // Validate the form and trigger userRegister
                       if (authCubit.singUpFormKey.currentState!.validate()) {
                         cubit.userRegister(
                           name: authCubit.name,
@@ -110,6 +132,8 @@ class CustomSignUpFormWidget extends StatelessWidget {
                     ),
                   ),
             const SizedBox(height: 20),
+
+            // AlreadyHaveAccountWidget for navigation to sign in
             AlreadyHaveAccountWidget(onTap: () {
               customNavigation(context, '/SignIn');
             })
