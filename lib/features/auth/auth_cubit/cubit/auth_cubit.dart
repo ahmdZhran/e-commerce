ba403635bfea5f9c4511c0cb89a5c3a733558f38
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-17 09:12:40 +0200

    add permisstion to andoid INTERNET

--- lib/features/auth/auth_cubit/cubit/auth_cubit.dart
+++ lib/features/auth/auth_cubit/cubit/auth_cubit.dart
@@ -62,7 +62,7 @@ class AuthCubit extends Cubit<AuthState> {
       image = File(pickedFile.path);
       bytes = File(image!.path).readAsBytesSync();
       userImage = base64Encode(bytes!);
-      print('images = $userImage');
+
       emit(ImageChoosen());
     } else {
       print('no image selected');
@@ -80,11 +80,9 @@ class AuthCubit extends Cubit<AuthState> {
       );
 
       userModel = UserModel.fromJson(response.data);
-      print(userModel!.user!.name);
 
       emit(LoginSuccess());
     } catch (error) {
-      print(error.toString());
       emit(LoginFailer(errMessage: error.toString()));
     }
   }
