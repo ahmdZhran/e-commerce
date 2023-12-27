12ae379e9e61d5d75c5d18ee6beaaec525130d45
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-27 22:41:27 +0200

    refactor our code

---
+++ lib/features/home/presentation/widgets/heart_icon_widget.dart
@@ -0,0 +1,23 @@
+import 'package:e_commerce/core/utls/app_assets.dart';
+import 'package:e_commerce/core/utls/app_color.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HeartIconWidget extends StatelessWidget {
+  const HeartIconWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      AppAssets.heart,
+      height: 24,
+      width: 24,
+      colorFilter: ColorFilter.mode(
+        AppColors.kPrimaryColor,
+        BlendMode.srcIn,
+      ),
+    );
+  }
+}
