37741f0fb93114a235bf12c3f8269cd122ee2170
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-28 07:33:42 +0200

    create list of prodcut item

---
+++ lib/features/home/presentation/widgets/list_of_product_item.dart
@@ -0,0 +1,25 @@
+import 'package:e_commerce/features/home/presentation/widgets/product_item.dart';
+import 'package:flutter/material.dart';
+
+class ListOfProductItem extends StatelessWidget {
+  const ListOfProductItem({super.key, required this.screenSize});
+
+  final Size screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: screenSize.height * 0.4,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 4,
+        itemBuilder: (BuildContext context, int index) {
+          return const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0),
+            child: ProductItem(),
+          );
+        },
+      ),
+    );
+  }
+}
