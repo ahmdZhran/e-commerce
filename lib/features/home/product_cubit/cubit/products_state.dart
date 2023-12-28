d670f4ec19d0e91d9f8f08b107880a1a8f7e6904
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-28 16:19:59 +0200

    add states of product in product satate

--- lib/features/home/product_cubit/cubit/products_state.dart
+++ lib/features/home/product_cubit/cubit/products_state.dart
@@ -4,3 +4,13 @@ part of 'products_cubit.dart';
 sealed class ProductState {}
 
 final class ProductInitial extends ProductState {}
+
+final class ProducLoading extends ProductState {}
+
+final class ProducSuccess extends ProductState {}
+
+final class ProducFailer extends ProductState {
+  final String errMessage;
+
+  ProducFailer({required this.errMessage});
+}
