257d24409bc43d9bfafff01c34f8df3bd3cc6a4b
Ahmed <ahmedmohameddx80@gmail.com>
2023-12-31 18:32:30 +0200

    add buildProductItem

--- lib/features/home/product_cubit/cubit/products_cubit.dart
+++ lib/features/home/product_cubit/cubit/products_cubit.dart
@@ -1,4 +1,5 @@
-import 'package:bloc/bloc.dart';
+import 'package:e_commerce/core/network/remote/dio_helper.dart';
+import 'package:e_commerce/features/home/data/models/products_model.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -7,4 +8,20 @@ part 'products_state.dart';
 class ProductCubit extends Cubit<ProductState> {
   ProductCubit() : super(ProductInitial());
   static ProductCubit get(context) => BlocProvider.of(context);
+  LaptopsModel? laptopsModel;
+
+  Future<void> getHomeProduct() async {
+    print('its loading now========');
+    emit(ProducLoading());
+
+    DioHelperStore.getData(url: 'https://elwekala.onrender.com/product/Laptops')
+        .then((value) {
+      laptopsModel = LaptopsModel.fromJson(value.data);
+
+      emit(ProducSuccess());
+    }).catchError((error) {
+      print(error.toString());
+      emit(ProducFailer(errMessage: error));
+    });
+  }
 }
--- lib/features/products/presentation/views/products_view.dart
+++ lib/features/products/presentation/views/products_view.dart
@@ -1,37 +1,53 @@
 import 'package:e_commerce/core/utls/app_strings.dart';
 import 'package:e_commerce/core/utls/texts_style.dart';
-import 'package:e_commerce/features/home/presentation/widgets/product_item.dart';
+import 'package:e_commerce/features/home/product_cubit/cubit/products_cubit.dart';
+import 'package:e_commerce/features/products/presentation/views/widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class ProductsView extends StatelessWidget {
   const ProductsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Text(
-              AppStrigns.yourProducts,
-              style: CustomTextStyle.medium14,
-            ),
-            // GridView
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
+    print('im in productView now ..........................................');
+    return BlocConsumer<ProductCubit, ProductState>(
+      listener: (context, state) {
+        if (state is ProducLoading) {
+          const CircularProgressIndicator();
+        }
+      },
+      builder: (context, state) {
+        var cubit = ProductCubit.get(context);
+        print(cubit.laptopsModel);
+        return Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: [
+                Text(
+                  AppStrigns.yourProducts,
+                  style: CustomTextStyle.medium14,
                 ),
-                itemBuilder: (context, index) {
-                  return const ProductItem(
-                    height: 110,
-                  );
-                },
-              ),
+                // GridView
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1 / 1.3,
+                    mainAxisSpacing: 1.0,
+                    crossAxisSpacing: 1.0,
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    children: List.generate(
+                        cubit.laptopsModel!.product!.length,
+                        (index) => buildProductItem(
+                            cubit.laptopsModel!.product![index], context)),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
